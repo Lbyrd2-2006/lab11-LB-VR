cf9ba84d82cd67f4f7eb26ec872760be37e5d169
Vitor Hugo Ribeiro <vitorhugoribeiro@MacBook-Air-2.local>
2025-11-19 22:56:19 -0500

    Partner 2 tests

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,16 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-3, 9), 6)
+        self.assertEqual(add(0, 0), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(5,3),2)
+        self.assertEqual(sub(10,3),7)
+        self.assertEqual(sub(2,-2), 4)
+
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +23,21 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0,5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(1000,10),3)
+        self.assertEqual(log(16,2),4)
+        self.assertEqual(log(1,10),0)
+
+
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(10,1)
+
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
